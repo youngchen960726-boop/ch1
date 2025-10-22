63c2bcbd86dd408adc3fd2a4632cd23fd695e938
youngchen960726 <youngchen960726@gmail.com>
2025-10-22 20:59:05 +0800

    first upload

---
+++ 2.25.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  CCCCCCCCC" << std::endl;
+    std::cout << " C         C" << std::endl;
+    std::cout << " C         C"<< std::endl;
+    std::cout << " C         C" << std::endl;
+    std::cout << " C         C" << std::endl;
+    std::cout << "  C       C" << std::endl;
+    std::cout << "   C     C" << std::endl;
+    std::cout << std::endl;
+    std::cout <<"RRRRRRRRRRRR" << std::endl;
+    std::cout << "     RR    R" << std::endl;
+    std::cout << "   R  R    R" << std::endl;
+    std::cout << " R    R    R" << std::endl;
+    std::cout << "R      RRRR" << std::endl;
+    std::cout << std::endl; 
+    std::cout << "           Y" << std::endl;
+    std::cout << "        Y" << std::endl;
+    std::cout << "     Y " << std::endl;
+    std::cout <<"YYYYYY  " << std::endl;
+    std::cout << "     Y  " << std::endl;
+    std::cout << "        Y" << std::endl;
+    std::cout << "           Y" << std::endl;
+
+    return 0;
+}
