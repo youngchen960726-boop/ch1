320cd94a08eeb725c7b182766a7e1a62c86fc124
youngchen960726-boop <youngchen960726@gmail.com>
2025-10-22 19:42:21 +0800

    Add files via upload

---
+++ 2.25.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"                 C    "<<endl;
+    cout <<"              C     C  "<<endl;
+    cout <<"            C         C"<<endl;
+    cout <<"          C             C"<<endl;
+    cout <<"        C                 C"<<endl;
+    cout <<"       C                    C"<<endl;
+    cout <<"      C                      C"<<endl;
+    cout <<"       C                    C"<<endl;
+    cout <<"                          "<<endl;
+    cout <<"      RRRRRRRRRRRRRRRRRRRRRRR    "<<endl;
+    cout <<"                  R R        R    "<<endl;
+    cout <<"                R   R        R    "<<endl;
+    cout <<"              R     R        R    "<<endl;
+    cout <<"            R       R        R    "<<endl;
+    cout <<"          R         R        R    "<<endl;
+    cout <<"        R           R        R    "<<endl;
+    cout <<"      R              RRRRRRR   "<<endl;
+    cout <<"                                  "<<endl;
+    cout <<"                             Y  "<<endl;
+    cout <<"                          Y        "<<endl;
+    cout <<"                       Y        "<<endl;
+    cout <<"                    Y   "<<endl;
+    cout <<"      YYYYYYYYYYYYYY        "<<endl;
+    cout <<"                    Y        "<<endl;
+    cout <<"                       Y        "<<endl;
+    cout <<"                          Y        "<<endl;
+    cout <<"                             Y        "<<endl;
+    return 0;
+}
